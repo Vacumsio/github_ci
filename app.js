86f45a7f0ec9852146f6d20855c714dc974e335b
Peter Gagarskiy <vacumsio@gmail.com>
2025-10-27 15:48:12 +0300

    linter add; change tab  4>2;

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 
 app.listen(5000, () => {
-    console.log('Server started at port 5000');
+  console.log('Server started at port 5000');
 });
